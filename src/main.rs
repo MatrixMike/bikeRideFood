b0a7146c9d81b7defa248eb39f19bb85bc7dcce7
Mike J H <matrixmike@icloud.com>
2019-11-26 15:26:11 +1100

    small steps

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,15 @@ use csv::Error;
 
 fn main() -> Result<(), Error> {
     let csv = "year,make,model,description
-1948,Porsche,356,Luxury sports car
-1967,Ford,Mustang fastback 1967,American car
-1999,Toyota,Corolla sedan,Japanese car";
+1948,Porsche,356,Luxury sports
+1967,Ford,Mustang fastback 1967,American
+1999,Toyota,Corolla sedan,Japanese";
 
     let mut reader = csv::Reader::from_reader(csv.as_bytes());
     for record in reader.records() {
         let record = record?;
         println!(
-            "In {}, {} built the {} model. It is a {}.",
+            "In {}, {} built the {} model. It is a {} car.",
             &record[0],
             &record[1],
             &record[2],
