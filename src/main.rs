ee63627ab4edfe957e79f9c46dae8523129b8e03
Mike J H <matrixmike@icloud.com>
2019-11-26 15:22:56 +1100

    adjustments for taste

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+extern crate csv;
+use csv::Error;
+
+fn main() -> Result<(), Error> {
+    let csv = "year,make,model,description
+1948,Porsche,356,Luxury sports car
+1967,Ford,Mustang fastback 1967,American car
+1999,Toyota,Corolla sedan,Japanese car";
+
+    let mut reader = csv::Reader::from_reader(csv.as_bytes());
+    for record in reader.records() {
+        let record = record?;
+        println!(
+            "In {}, {} built the {} model. It is a {}.",
+            &record[0],
+            &record[1],
+            &record[2],
+            &record[3]
+        );
+    }
+
+    Ok(())
+}
+
